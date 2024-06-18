7d7175b094968e7fe0052cbde947302a88a03a2e
SarabojDev <saraDev@email.com>
2024-06-19 04:45:53 +0530

    adding datase and api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((s) => {
+    console.log("Databse connected");
+  }).catch(error=>{console.log(error.message)});
 
 const app = express();
 
